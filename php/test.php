7db5f0d851c62cc4ea800cfb9fcd9791c091fe6d
bayuhafiz <bayu@colorblindlabs.com>
2015-09-15 13:49:56 +0700

    updated: db connection

--- php/test.php
+++ php/test.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $mysqli_connection = new MySQLi('localhost', 'getbandits_user', 'Hello123#', 'getbandits_db');
 if ($mysqli_connection->connect_error) {
    echo "Not connected, error: " . $mysqli_connection->connect_error;
