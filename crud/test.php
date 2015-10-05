d7690908ddb374b7a4d6620e6ee3944a7bd23958
bayuhafiz <bayu@colorblindlabs.com>
2015-10-05 12:56:45 +0700

    flush

--- crud/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli_connection = new MySQLi('localhost', 'getbandits_user', 'Hello123#', 'getbandits_db');
-if ($mysqli_connection->connect_error) {
-   echo "Not connected, error: " . $mysqli_connection->connect_error;
-}
-else {
-   echo "Connected.";
-}
-?>
